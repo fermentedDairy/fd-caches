23c54a83553701ca11d31ad7441b3c283ba13e3d
edward cheesman <fermentedDairy@gmail.com>
2023-08-05 17:26:52 +0200

    Concurrent Load Attempt Test Authored And Passing

--- microprofile-caches-cdi/src/main/java/org/fermented/dairy/microprofile/caches/core/providers/HashMapCache.java
+++ microprofile-caches-cdi/src/main/java/org/fermented/dairy/microprofile/caches/core/providers/HashMapCache.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import org.fermented.dairy.microprofile.caches.api.exceptions.CacheException;
 import org.fermented.dairy.microprofile.caches.api.functions.Loader;
 import org.fermented.dairy.microprofile.caches.api.functions.OptionalLoader;
@@ -27,29 +28,26 @@ public final class HashMapCache implements Cache {
 
         final ConcurrentHashMap<Object, SoftReference<CacheEntry>> cache = cacheHolder.cache();
         SoftReference<CacheEntry> entryReference;
-        if (!cache.containsKey(key) //Key is not present
-                || (entryReference = cache.get(key)) == null //mapped value is null
+        if ((entryReference = cache.get(key)) == null //mapped value is null
                 || entryReference.get() == null //SoftReference that's mapped has been GCed
                 || entryReference.get().isExpired()) //cached value has expired
         {
+
             entryReference = new SoftReference<>(
                     new CacheEntry(ttl)
             );
-            cache.put(key, entryReference);
-            synchronized (entryReference.get()) {
-                final Object value;
-                if ((value = loader.load(key)) == null) {
-                    cache.remove(key);
-                    return null;
-                }
-                validateResultClass(value, valueClass, cacheHolder, cacheName);
-                entryReference.get().setValue(loader.load(key));
-            }
+            return loadValueIntoCache(key, loader, cacheName, valueClass, cacheHolder, cache, entryReference);
         }
-        return entryReference.get().getValue();
+        entryReference.get().readLock().lock();
+        try {
+            return entryReference.get().getValue();
+        } finally {
+            entryReference.get().readLock().unlock();
+        }
     }
+
     @Override
-    public Object loadOptional(final Object key, final OptionalLoader<Object, Object> loader, final String cacheName, final long ttl, final Class keyClass, final Class valueClass)  throws Exception {
+    public Object loadOptional(final Object key, final OptionalLoader<Object, Object> loader, final String cacheName, final long ttl, final Class keyClass, final Class valueClass) throws Exception {
         return null;
     }
 
@@ -77,14 +75,14 @@ public final class HashMapCache implements Cache {
     public Collection<Object> getKeys(final String cacheNames) {
         final CacheHolder cacheHolder;
 
-        if((cacheHolder = CACHES.get(cacheNames)) == null) return Collections.emptySet();
+        if ((cacheHolder = CACHES.get(cacheNames)) == null) return Collections.emptySet();
 
-        final  ConcurrentHashMap<Object, SoftReference<CacheEntry>> cache = cacheHolder.cache();
+        final ConcurrentHashMap<Object, SoftReference<CacheEntry>> cache = cacheHolder.cache();
         cache.entrySet().stream()
                 .filter(
-                entry -> entry.getValue().get() == null ||
-                        entry.getValue().get().isExpired()
-        ).forEach(entry -> cache.remove(entry.getKey()));
+                        entry -> entry.getValue().get() == null ||
+                                entry.getValue().get().isExpired()
+                ).forEach(entry -> cache.remove(entry.getKey()));
         return cache.keySet();
     }
 
@@ -93,7 +91,7 @@ public final class HashMapCache implements Cache {
         return getClass().getCanonicalName();
     }
 
-    private CacheHolder getCache(final String cacheName, Class keyClass, Class resultClass) {
+    private CacheHolder getCache(final String cacheName, final Class keyClass, final Class resultClass) {
         return CACHES.computeIfAbsent(cacheName, key -> new CacheHolder(
                 new ConcurrentHashMap<>(),
                 keyClass,
@@ -103,24 +101,44 @@ public final class HashMapCache implements Cache {
 
     private void validateKeyClass(final Object key, final Class keyClass, final CacheHolder cacheHolder, final String cacheName) {
         final Object nonNullKey = Objects.requireNonNull(key, "the key cannot be null");
-        if(!keyClass.isInstance(nonNullKey) || !cacheHolder.keyClass().isInstance(nonNullKey)) {
+        if (!keyClass.isInstance(nonNullKey) || !cacheHolder.keyClass().isInstance(nonNullKey)) {
             throw new CacheException("%s is not a valid key class for cache %s", nonNullKey.getClass().getCanonicalName(), cacheName);
         }
     }
 
     private void validateResultClass(final Object result, final Class resultClass, final CacheHolder cacheHolder, final String cacheName) {
-        if(!resultClass.isInstance(result) || !cacheHolder.resultClass().isInstance(result)) {
+        if (!resultClass.isInstance(result) || !cacheHolder.resultClass().isInstance(result)) {
             throw new CacheException("%s is not a valid result class for cache %s", result.getClass().getCanonicalName(), cacheName);
         }
     }
 
+    private Object loadValueIntoCache(final Object key, final Loader<Object, Object> loader, final String cacheName, final Class valueClass, final CacheHolder cacheHolder, final ConcurrentHashMap<Object, SoftReference<CacheEntry>> cache, final SoftReference<CacheEntry> entryReference) throws Exception {
+        entryReference.get().writeLock().lock();
+        try {
+            if (entryReference.get().getValue() != null)
+                return entryReference.get().getValue();
+            cache.put(key, entryReference);
+            final Object value;
+            if ((value = loader.load(key)) == null) {
+                cache.remove(key);
+                return null;
+            }
+            validateResultClass(value, valueClass, cacheHolder, cacheName);
+            entryReference.get().setValue(loader.load(key));
+            return entryReference.get().getValue();
+        } finally {
+            entryReference.get().writeLock().unlock();
+        }
+    }
 
-    private static class CacheEntry {
-        private final long expiryTime;
-        private Object value = null;
+    private static final class CacheEntry extends ReentrantReadWriteLock {
+        private final long ttl;
+        private long expiryTime;
+        private Object value; //NOSONAR: java:S1948, not planning on any serialisation
 
-        public CacheEntry(long ttl) {
-            this.expiryTime = System.currentTimeMillis() + ttl;
+        public CacheEntry(final long ttl) {
+            this.ttl = ttl;
+            expiryTime = System.currentTimeMillis() + ttl;
         }
 
         public boolean isExpired() {
@@ -133,13 +151,37 @@ public final class HashMapCache implements Cache {
 
         public void setValue(final Object value) {
             if (this.value != null) throw new CacheException("values can only be set once");
+            expiryTime = System.currentTimeMillis() + ttl; //refresh expiry time after value has been set
             this.value = value;
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            final CacheEntry that = (CacheEntry) o;
+            return expiryTime == that.expiryTime && Objects.equals(value, that.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(expiryTime, value);
+        }
+
+        @Override
+        public String toString() {
+            return "CacheEntry{" +
+                    "expiryTime=" + expiryTime +
+                    ", currentTime=" + System.currentTimeMillis() +
+                    ", isExpired=" + isExpired() +
+                    ", value=" + value +
+                    '}';
         }
     }
 
     private record CacheHolder(
             ConcurrentHashMap<Object, SoftReference<CacheEntry>> cache,
-            Class keyClass, Class resultClass){
+            Class keyClass, Class resultClass) {
 
     }
 }
--- microprofile-caches-cdi/src/test/java/org/fermented/dairy/microprofile/caches/core/providers/HashMapCacheProviderTest.java
+++ microprofile-caches-cdi/src/test/java/org/fermented/dairy/microprofile/caches/core/providers/HashMapCacheProviderTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import org.fermented.dairy.microprofile.caches.api.exceptions.CacheException;
 import org.fermented.dairy.microprofile.caches.api.functions.Loader;
 import org.fermented.dairy.microprofile.caches.api.interfaces.Cache;
@@ -19,17 +21,17 @@ class HashMapCacheProviderTest {
     Cache provider = new HashMapCache();
 
     @BeforeEach
-    void beforeEach(){
+    void beforeEach() {
         provider.purge();
         assertTrue(provider.getCacheNames().isEmpty());
     }
 
     @DisplayName("""
-    with an empty cache\s
-    given an initial load (non-optional)\s
-    then second load should return the result of the first load\s
-    (load cache miss followed by cache hit)
-    """)
+            with an empty cache\s
+            given an initial load (non-optional)\s
+            then second load should return the result of the first load\s
+            (load cache miss followed by cache hit)
+            """)
     @Test
     void cacheMissFollowedByCacheHit() throws Exception {
         final Loader<Object, Object> firstLoader = key -> "Number Loaded Into Cache: " + key;
@@ -44,11 +46,11 @@ class HashMapCacheProviderTest {
     }
 
     @DisplayName("""
-    with an empty cache\s
-    given an initial load (non-optional) that returns null from loader\s
-    then null should be returned and nothing cached\s
-    (load cache miss null from loader)
-    """)
+            with an empty cache\s
+            given an initial load (non-optional) that returns null from loader\s
+            then null should be returned and nothing cached\s
+            (load cache miss null from loader)
+            """)
     @Test
     void cacheMissFNullFromLoader() throws Exception {
         final Loader<Object, Object> firstLoader = key -> null;
@@ -60,11 +62,11 @@ class HashMapCacheProviderTest {
     }
 
     @DisplayName("""
-    with an empty cache\s
-    given an initial load (non-optional), after the TTL has elapsed\s
-    then second load should return the result of the first load\s
-    (load cache miss followed by cache miss, expiry on load)
-    """)
+            with an empty cache\s
+            given an initial load (non-optional), after the TTL has elapsed\s
+            then second load should return the result of the first load\s
+            (load cache miss followed by cache miss, expiry on load)
+            """)
     @Test
     void cacheMissFollowedByCacheHitExpiryOnLoad() throws Exception {
         final Loader<Object, Object> firstLoader = key -> "Number Loaded Into Cache: " + key;
@@ -74,7 +76,7 @@ class HashMapCacheProviderTest {
                 () -> assertEquals(firstLoader.load(1L), result1),
                 () -> assertEquals(Set.of("NumberCache"), provider.getCacheNames()),
                 () -> assertEquals(Set.of(1L), provider.getKeys("NumberCache")));
-        Thread.sleep(50L);
+        Thread.sleep(50L); //NOSONAR: TODO: is there a better way?
         final String result2 = (String) provider.load(1L, secondLoader, "NumberCache", 3000, Long.class, String.class);
         assertAll("assert load results and cache state",
                 () -> assertEquals(secondLoader.load(1L), result2),
@@ -83,50 +85,74 @@ class HashMapCacheProviderTest {
     }
 
     @DisplayName("""
-    with an empty cache\s
-    given an initial load (non-optional), after the TTL has elapsed\s
-    then state interrogation reflect expiry\s
-    (load cache miss followed by cache miss, expiry on state interrogation)
-    """)
+            with an empty cache\s
+            given an initial load (non-optional), after the TTL has elapsed\s
+            then state interrogation reflect expiry\s
+            (load cache miss followed by cache miss, expiry on state interrogation)
+            """)
     @Test
     void cacheMissFollowedByCacheHitExpiryOnStateInterrogation() throws Exception {
         final Loader<Object, Object> firstLoader = key -> "Number Loaded Into Cache: " + key;
-        final Loader<Object, Object> secondLoader = key -> "Number Should Come From Cache: " + key;
         final String result1 = (String) provider.load(1L, firstLoader, "NumberCache", 30, Long.class, String.class);
         assertAll("assert initial load results and cache state",
                 () -> assertEquals(firstLoader.load(1L), result1),
                 () -> assertEquals(Set.of("NumberCache"), provider.getCacheNames()),
                 () -> assertEquals(Set.of(1L), provider.getKeys("NumberCache")));
-        Thread.sleep(50L);
+        Thread.sleep(50L); //NOSONAR: TODO: Is there a better way?
         assertAll("assert initial load results and cache state",
                 () -> assertTrue(provider.getKeys("NumberCache").isEmpty()));
     }
 
     @DisplayName("""
-    with an empty cache\s
-    given an initial load (non-optional) with incorrect key type\s
-    then throw an exception\s
-    (load cache miss followed by cache hit)
-    """)
+            with an empty cache\s
+            given an initial load (non-optional) with incorrect key type\s
+            then throw an exception\s
+            (load cache miss followed by cache hit)
+            """)
     @Test
-    void cacheMissFollowedInvalidKeyTypeException() throws Exception {
+    void cacheMissFollowedInvalidKeyTypeException() {
         final Loader<Object, Object> firstLoader = key -> "Number Loaded Into Cache: " + key;
-        final Loader<Object, Object> secondLoader = key -> "Number Should Come From Cache: " + key;
-        final String result1 = (String) provider.load(1L, firstLoader, "NumberCache", 3000, Long.class, String.class);
-        CacheException exception = assertThrows(CacheException.class, () -> provider.load("1", secondLoader, "NumberCache", 3000, Long.class, String.class));
+        final CacheException exception = assertThrows(CacheException.class, () -> provider.load("1", firstLoader, "NumberCache", 3000, Long.class, String.class));
         assertEquals("java.lang.String is not a valid key class for cache NumberCache", exception.getMessage());
     }
 
     @DisplayName("""
-    with an empty cache\s
-    given an initial load (non-optional) with incorrect return type\s
-    then throw an exception\s
-    (load cache miss followed by cache hit)
-    """)
+            with an empty cache\s
+            given an initial load (non-optional) with incorrect return type\s
+            then throw an exception\s
+            (load cache miss followed by cache hit)
+            """)
     @Test
-    void cacheMissFollowedInvalidResultTypeException() throws Exception {
+    void cacheMissFollowedInvalidResultTypeException() {
         final Loader<Object, Object> loader = key -> Math.PI;
         final CacheException exception = assertThrows(CacheException.class, () -> provider.load(1L, loader, "NumberCache", 3000, Long.class, String.class));
         assertEquals("java.lang.Double is not a valid result class for cache NumberCache", exception.getMessage());
+    }
+
+    @DisplayName("""
+            with an empty cache\s
+            given an initial load (non-optional) concurrently with a second load\s
+            then the first load value should be loaded into the cache and returned both times\s
+            (concurrent load with same key thread locking with same key)
+            """)
+    @Test
+    void concurrentLoadWithSameKeyBlocksAllThreads() throws Exception {
+        final Loader<Object, Object> firstLoader = key -> {
+            Thread.sleep(1000L); //NOSONAR: java:S2925 simulating latency in load function
+            return "Number Loaded Into Cache: " + key;
+        };
+        final Loader<Object, Object> secondLoader = key -> "Number Should Come From Cache: " + key;
+
+        final Future<String> futureResult1 = Executors.newSingleThreadExecutor().submit(
+                () -> (String) provider.load(1L, firstLoader, "NumberCache", 3000, Long.class, String.class));
+        Thread.sleep(50L); ///NOSONAR: TODO: Is there a better way?
+        final String result2 = (String) provider.load(1L, secondLoader, "NumberCache", 3000, Long.class, String.class);
+        final String result1 = futureResult1.get();
+        assertAll("assert load results and cache state",
+                () -> assertEquals(firstLoader.load(1L), result1),
+                () -> assertEquals(firstLoader.load(1L), result2),
+                () -> assertEquals(Set.of("NumberCache"), provider.getCacheNames()),
+                () -> assertEquals(Set.of(1L), provider.getKeys("NumberCache")));
+
     }
 }

72aa769f8bcd9e282da4f6fb4d5e74ae0f49f224
edward cheesman <fermentedDairy@gmail.com>
2023-08-14 21:13:54 +0200

    Removing superfluous requires in module-info.java

--- microprofile-caches-api/src/main/java/module-info.java
+++ microprofile-caches-api/src/main/java/module-info.java
@@ -6,7 +6,6 @@
  * exports - org.fermented.dairy.microprofile.caches.api.interfaces
  */
 module microprofile.caches.api {
-    requires jakarta.interceptor;
     exports org.fermented.dairy.microprofile.caches.api.exceptions;
     exports org.fermented.dairy.microprofile.caches.api.functions;
     exports org.fermented.dairy.microprofile.caches.api.interfaces;
